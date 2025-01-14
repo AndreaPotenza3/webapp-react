9f35d0ec68d330e0a0144e3db80fc7e35d26afdc
AndreaPotenza <andreapoten3@gmail.com>
2025-01-14 23:20:17 +0100

    footer

--- Layout/Footer.jsx
+++ Layout/Footer.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router";
+
 export default function Footer() {
-    return <div className="py-4 px-2 bg-dark text-white text-center">Footer</div>
+    return (
+        <div className="py-4 px-2 bg-dark text-white text-center">
+            {!'/' ? <Link to="/">Torna alla Home</Link> : ''}
+        </div>
+    )
 }
